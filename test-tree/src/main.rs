5a179f4d5651f912f910202908911b6a5d9bedc3
Tomas Chabada <tchabada@gmail.com>
2020-05-03 20:29:30 +0200

    test-tree update

--- test-tree/src/main.rs
+++ test-tree/src/main.rs
@@ -48,6 +48,63 @@ impl<T: Ord> AvlTreeSet<T> {
     }
 }
 
+#[derive(Debug)]
+struct AvlTreeSetIter<'a, T: Ord> {
+    prev_nodes: Vec<&'a AvlNode<T>>,
+    current_tree: &'a AvlTree<T>,
+}
+
+impl<'a, T: 'a + Ord> Iterator for AvlTreeSetIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match *self.current_tree {
+                None => match self.prev_nodes.pop() {
+                    None => {
+                        return None;
+                    }
+
+                    Some(ref prev_node) => {
+                        self.current_tree = &prev_node.right;
+
+                        return Some(&prev_node.value);
+                    }
+                },
+
+                Some(ref current_node) => {
+                    if current_node.left.is_some() {
+                        self.prev_nodes.push(&current_node);
+                        self.current_tree = &current_node.left;
+
+                        continue;
+                    }
+
+                    if current_node.right.is_some() {
+                        self.current_tree = &current_node.right;
+
+                        return Some(&current_node.value);
+                    }
+
+                    self.current_tree = &None;
+
+                    return Some(&current_node.value);
+                }
+            }
+        }
+    }
+}
+
+// Addition of lifetime parameter for the set
+impl<'a, T: 'a + Ord> AvlTreeSet<T> {
+    fn iter(&'a self) -> AvlTreeSetIter<'a, T> {
+        AvlTreeSetIter {
+            prev_nodes: Vec::new(),
+            current_tree: &self.root,
+        }
+    }
+}
+
 fn main() {
     let tree = Some(Box::new(AvlNode {
         value: 2,
@@ -92,6 +149,18 @@ fn main() {
             })),
         }))
     );
+
+    let mut set = AvlTreeSet::new();
+
+    for i in (1..4 as usize).rev() {
+        set.insert(i);
+    }
+
+    let mut iter = set.iter();
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), None);
 }
 
 #[cfg(test)]
